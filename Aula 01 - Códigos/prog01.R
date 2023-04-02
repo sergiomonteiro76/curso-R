58c0b4eced71252b55483294e19702130e9dd01c
Sérgio Assunção Monteiro <98135001+sergiomonteiro76@users.noreply.github.com>
2023-04-02 07:11:47 -0300

    Update prog01.R

--- Aula 01 - Códigos/prog01.R
+++ Aula 01 - Códigos/prog01.R
@@ -4,9 +4,11 @@
 #linkedin: https://www.linkedin.com/in/sergio-assun%C3%A7%C3%A3o-monteiro-b781897b/
 #lattes: http://lattes.cnpq.br/9489191035734025
 #----------------------------------------------------------------------
+cat("\014") #limpar console ou pressionar Ctrl+L
 x <- c(10.7, 5.36, 43.1, 8.41, 91.7)
 print("Forma 1 de imprimir: ")
 print("valores de x: ")
 print(x)
 print("Forma 2 de imprimir: ")
-message('valores de x: ', x)
+print("valores de x: ")
+str(x)
